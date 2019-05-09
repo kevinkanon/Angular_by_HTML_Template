dacdcf3df0a381e7e2d28cefd109bec457521f06
Kevin Kanon <kevin.kanon@st-groupe.fr>
2019-05-09 17:08:26 +0200

    gallery component ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
